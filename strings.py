f45cfb8f0ddbc8761e63b15cd24d6c3dd0665ded
Michael Silva <mike.a.silva@gmail.com>
2023-12-24 07:44:09 -0500

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,11 @@
+# Let's take some text
+text = "Boaty McBoatface"
+
+# What happens when we split it on Mc?
+print(text.split("Mc"))
+
+# How about Boat?
+print(text.split("Boat"))
+
+# What about Chihuahua?
+print(text.split("Chihuahua"))
